1b9578efb41f2cd8f83ee8c169f27e34ccbc1986
ssc0ttie <ksmacawile@gmail.com>
2025-08-22 17:54:28 +0800

    remove supabase again

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
-from supabase import create_client, Client
-from dotenv import load_dotenv
+
+# from supabase import create_client, Client
+# from dotenv import load_dotenv
 import os
 
 
